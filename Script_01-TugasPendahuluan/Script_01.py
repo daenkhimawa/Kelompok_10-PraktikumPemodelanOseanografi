002a28a4f4586ac15f8b27c39a9e6f7be8d1c611
winhendra2002 <105977853+winhendra2002@users.noreply.github.com>
2022-05-23 13:23:56 +0700

    Update Script_01.py

--- Script_01-TugasPendahuluan/Script_01.py
+++ Script_01-TugasPendahuluan/Script_01.py
@@ -1,2 +1,1 @@
-#Script 01: Tugas Pendahuluan
-#Di sini kalian nulis script nya ya, silakan
+pada praktikum pemodelan oseanografi tahun 2022 ini, terdapat 4 modul, yaitu modul 1 berupa modul pendahuluan, modul 2 berupa 

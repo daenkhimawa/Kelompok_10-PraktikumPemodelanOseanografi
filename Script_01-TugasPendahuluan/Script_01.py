a6ed81212546c5bb72e9c6bbd57b604deb941b2f
winhendra2002 <105977853+winhendra2002@users.noreply.github.com>
2022-05-23 13:26:19 +0700

    Update Script_01.py

--- Script_01-TugasPendahuluan/Script_01.py
+++ Script_01-TugasPendahuluan/Script_01.py
@@ -1,1 +1,2 @@
-pada praktikum pemodelan oseanografi tahun 2022 ini, terdapat 4 modul, yaitu modul 1 berupa modul pendahuluan, modul 2 berupa 
+pada praktikum pemodelan oseanografi tahun 2022 ini, terdapat 4 modul, yaitu modul 1 berupa modul pendahuluan, modul 2 berupa adveksi difusi 2D, 
+modul 3 berupa model hidrodinamika 1D sederhana, dan modul 4 berupa model hidrodinamika 2D.  
